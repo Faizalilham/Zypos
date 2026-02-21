b7ae60b421357a61ff2207c7c4ef6e091f1314e9
Faizalilham <faizalfalakh19@gmail.com>
2026-02-21 21:51:03 +0700

    Fixing instrumentation test

--- app/src/androidTest/java/dev/faizal/zypos/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/faizal/zypos/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.faizal.zypos", appContext.packageName)
+        assertEquals("dev.faizal.zypos.dev", appContext.packageName)
     }
 }
