493fcbdc42912779fdd492db4f5afb44872fe4a3
Faizalilham <faizalfalakh19@gmail.com>
2026-05-06 18:56:45 +0700

    deploy new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.faizal.zypos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
